49cdcc0698af19557e1aab85b5a6e2d60a016b58
张桐维 <cyberstratus@zhangtongweideMacBook-Pro.local>
2022-07-13 09:02:50 -0400

    comment destroy and update api

---
+++ comments/api/permissions.py
@@ -0,0 +1,13 @@
+from rest_framework.permissions import BasePermission
+
+class IsObjectOwner(BasePermission):
+    """
+    Permission checks obj.user == request.user
+    """
+    message = "You do not have permission to access this object"
+
+    def has_permission(self, request, view):
+        return True
+
+    def has_object_permission(self, request, view, obj):
+        return request.user == obj.user
--- comments/api/views.py
+++ comments/api/views.py
@@ -5,7 +5,9 @@ from comments.models import Comment
 from comments.api.serializers import (
     CommentSerializer,
     CommentSerializerForCreate,
+    CommentSerializerForUpdate,
 )
+from comments.api.permissions import IsObjectOwner
 
 
 class CommentViewSet(viewsets.GenericViewSet):
@@ -19,6 +21,8 @@ class CommentViewSet(viewsets.GenericViewSet):
         # use AllowAny() / IsAuthenticated() to create obj
         if self.action == 'create':
             return [IsAuthenticated()]
+        if self.action in ['destroy', 'update']:
+            return [IsAuthenticated(), IsObjectOwner()]
         return [AllowAny()]
 
     def create(self, request, *args, **kwargs):
@@ -41,3 +45,29 @@ class CommentViewSet(viewsets.GenericViewSet):
             CommentSerializer(comment).data,
             status=status.HTTP_201_CREATED,
         )
+
+    def update(self, request, *args, **kwargs):
+        # get_object is a function wrapped by DRF，will raise 404 error when not found
+        serializer = CommentSerializerForUpdate(
+            instance=self.get_object(),
+            data=request.data,
+        )
+        if not serializer.is_valid():
+            return Response({
+                'message': 'Please check input',
+                'errors': serializer.errors,
+            }, status=status.HTTP_400_BAD_REQUEST)
+        # save method will activate serializer's update method
+        # save follows instance's parameter to activate create or update
+        comment = serializer.save()
+        return Response(
+            CommentSerializer(comment).data,
+            status=status.HTTP_200_OK,
+        )
+
+    def destroy(self, request, *args, **kwargs):
+        comment = self.get_object()
+        comment.delete()
+        # DRF's default return value of destroy is status code = 204 no content
+        # here returns success=True more straight foward so return 200 more suitable
+        return Response({'success': True}, status=status.HTTP_200_OK)

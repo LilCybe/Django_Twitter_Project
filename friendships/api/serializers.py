50f7161e8e61d487a210aac609239c3337655370
张桐维 <cyberstratus@zhangtongweideMacBook-Pro.local>
2022-08-11 02:03:32 -0400

    friendship pagination

--- friendships/api/serializers.py
+++ friendships/api/serializers.py
@@ -1,24 +1,21 @@
 from accounts.api.serializers import UserSerializerForFriendship
 from friendships.models import Friendship
+from friendships.services import FriendshipService
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
-
 
 class FriendshipSerializerForCreate(serializers.ModelSerializer):
     from_user_id = serializers.IntegerField()
     to_user_id = serializers.IntegerField()
-
     class Meta:
         model = Friendship
         fields = ('from_user_id', 'to_user_id')
-
     def validate(self, attrs):
         if attrs['from_user_id'] == attrs['to_user_id']:
             raise ValidationError({
                 'message': 'from_user_id and to_user_id should be different'
             })
         return attrs
-
     def create(self, validated_data):
         from_user_id = validated_data['from_user_id']
         to_user_id = validated_data['to_user_id']
@@ -34,16 +31,30 @@ class FriendshipSerializerForCreate(serializers.ModelSerializer):
 class FollowerSerializer(serializers.ModelSerializer):
     user = UserSerializerForFriendship(source='from_user')
     created_at = serializers.DateTimeField()
+    has_followed = serializers.SerializerMethodField()
 
     class Meta:
         model = Friendship
-        fields = ('user', 'created_at')
+        fields = ('user', 'created_at', 'has_followed')
+
+    def get_has_followed(self, obj):
+        if self.context['request'].user.is_anonymous:
+            return False
+
+        return FriendshipService.has_followed(self.context['request'].user, obj.from_user)
 
 
 class FollowingSerializer(serializers.ModelSerializer):
     user = UserSerializerForFriendship(source='to_user')
     created_at = serializers.DateTimeField()
+    has_followed = serializers.SerializerMethodField()
 
     class Meta:
         model = Friendship
-        fields = ('user', 'created_at')
+        fields = ('user', 'created_at', 'has_followed')
+
+    def get_has_followed(self, obj):
+        if self.context['request'].user.is_anonymous:
+            return False
+
+        return FriendshipService.has_followed(self.context['request'].user, obj.to_user)
